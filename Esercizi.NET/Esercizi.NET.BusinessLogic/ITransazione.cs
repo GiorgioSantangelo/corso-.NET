b61501c1977c5d8ea759c0a3725965c56077da0f
GiorgioSantangelo24 <giorgio.santangelo.24@gmail.com>
2019-02-04 15:51:12 +0100

    commit 2

---
+++ Esercizi.NET/Esercizi.NET.BusinessLogic/ITransazione.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizi.NET.BusinessLogic
+{
+   
+          public interface ITransazione
+             {
+            string Tipo { get; set; }
+            string Categoria { get; set; }
+            string Descrizione { get; set; }
+            DateTime DataTransazione { get; set; }
+            decimal Importo { get; set; }
+            }
+
+}
